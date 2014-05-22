5c691c772796243674c2be8243743d217c5ecda3
Tobias Pfaff <tpfaff@berkeley.edu>
2014-05-21 19:27:24 -0700

    include upstream fixes into mouse.lab

--- script/pack.sh
+++ script/pack.sh
@@ -6,10 +6,10 @@ delta=dists/engine-data/patches/grim_pnc_delta
 lab=dists/engine-data/mouse.lab
 
 mkdir -p $tmpdir
+rm -rf $tmpdir/*
 $tooldir/luac -o $delta/mouse.lua $grimex/mouse.lua
 cp $delta/* $tmpdir/
 python script/convert_to_bin.py
 python script/patch_lua.py
 $tooldir/mklab --grim $tmpdir $lab
 cp $lab .
-rm -rf $tmpdir
