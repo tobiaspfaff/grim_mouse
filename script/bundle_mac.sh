f2afcca1033cb9dc6c47ba917749616918f98678
Tobias Pfaff <tpfaff@berkeley.edu>
2014-06-21 23:52:26 -0700

    rebased, v0.7

--- script/bundle_mac.sh
+++ script/bundle_mac.sh
@@ -8,5 +8,5 @@ cp README.ResidualVM /tmp/pkg/Readme_ResidualVM.txt
 cp README /tmp/pkg/Readme.txt
 hdiutil create /tmp/grim.dmg -ov -volname "Grim Mouse" -fs HFS+ -srcfolder /tmp/pkg
 rm ~/Downloads/grim*.dmg
-hdiutil convert /tmp/grim.dmg -format UDZO -o ~/Downloads/grim_mouse_0_6.dmg
+hdiutil convert /tmp/grim.dmg -format UDZO -o ~/Downloads/grim_mouse_0_7.dmg
 scp ~/Downloads/grim_*.dmg login.eecs.berkeley.edu:public_html/data/
